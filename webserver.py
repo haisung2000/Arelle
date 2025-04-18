9601847f9789b933b6d416c9b54de528a2febb43
haisung2000 <haisung2000@naver.com>
2025-04-18 13:53:16 +0900

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -1,5 +1,5 @@
 # webserver.py
 from arelle import Cntlr
 
-Cntlr.Cntlr(logFileName='logToPrint', logFileMode='w', logLevel='debug', serverMode=True, host='0.0.0.0', port=8080)
+Cntlr.Cntlr(logFileName='logToPrint', logFileMode='w', serverMode=True, host='0.0.0.0', port=8080)
 
