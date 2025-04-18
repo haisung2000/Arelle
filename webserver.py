1b2702a013cc64affa55b091b409fbf3da824974
haisung2000 <haisung2000@naver.com>
2025-04-18 13:48:04 +0900

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -1,3 +1,5 @@
 # webserver.py
-from arelle import CntlrWebMain
-CntlrWebMain.CntlrWebMain(serverMode=True, host='0.0.0.0', port=8080)
+from arelle import Cntlr
+
+Cntlr.Cntlr(logFileName='logToPrint', logFileMode='w', logLevel='debug', serverMode=True, host='0.0.0.0', port=8080)
+
